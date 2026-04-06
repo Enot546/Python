18929d87039de183a95d2192c14b5025d5b93cfe
Enot546 <vityusha.matveev.1983@mail.ru>
2026-04-06 21:00:54 +0800

    commit 5.2

--- 03_lesson/address.py
+++ 03_lesson/address.py
@@ -2,21 +2,20 @@ class Address:
     index = "000000"
     city = "name"
     street = "name"
-    home_number = "00"
-    apart_number = "00"
-    to_address = (101000, "Москва", "Невская", 28, 10)
-    from_address = (354000, "Сочи", "Ленина", 45, 13)
+    home = "00"
+    flat = "00"
 
-    def __init__(self, index, city, street, home, flat, to_address,
-                 from_address):
+    def __init__(self, index, city, street, home, flat):
         self.index = index
         self.city = city
         self.street = street
         self.home = home
         self.flat = flat
-        to_address = to_address
-        from_address = from_address
 
     def __str__(self):
         return (f"{self.index}, {self.street}, "
                 f"{self.home},  {self.flat}")
+
+
+to_address = Address(101000, "Москва", "Невская", 28, 10)
+from_address = Address(354000, "Сочи", "Ленина", 45, 13)
--- 03_lesson/lesson_3_task_3.py
+++ 03_lesson/lesson_3_task_3.py
@@ -1,10 +1,8 @@
 from address import Address
 from mailing import Mailing
 
-to_address = Address
-from_address = Address
-to_address = 101000, "Москва", "Невская", 28, 10
-from_address = 354000, "Сочи", "Ленина", 45, 13
+to_address = Address(101000, "Москва", "Невская", 28, 10)
+from_address = Address(354000, "Сочи", "Ленина", 45, 13)
 mailing = Mailing(to_address, from_address, 3000, '81726354')
 
 print(mailing)
