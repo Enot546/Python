709c74ae236554bb093a2238b4bac1edf44d70ac
Enot546 <vityusha.matveev.1983@mail.ru>
2026-04-06 20:48:23 +0800

    commit 5.1

--- 03_lesson/address.py
+++ 03_lesson/address.py
@@ -4,21 +4,19 @@ class Address:
     street = "name"
     home_number = "00"
     apart_number = "00"
+    to_address = (101000, "Москва", "Невская", 28, 10)
+    from_address = (354000, "Сочи", "Ленина", 45, 13)
 
-    def __init__(self, index, city, street, home, flat):
+    def __init__(self, index, city, street, home, flat, to_address,
+                 from_address):
         self.index = index
         self.city = city
         self.street = street
         self.home = home
         self.flat = flat
+        to_address = to_address
+        from_address = from_address
 
     def __str__(self):
-        return (f"{self.index}, {self.street}, дом "
-                f"{self.home} - квартира {self.flat}")
-
-    def to_address(self):
-        print(self.to_address)
-
-
-to_address = (101000, "Москва", "Невская", 28, 10)
-from_address = (354000, "Сочи", "Ленина", 45, 13)
+        return (f"{self.index}, {self.street}, "
+                f"{self.home},  {self.flat}")
