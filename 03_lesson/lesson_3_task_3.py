ddc83c87fe01b669bab1bec9c37f8acfe77062d9
Enot546 <vityusha.matveev.1983@mail.ru>
2026-04-10 16:29:35 +0800

    commit

--- 03_lesson/lesson_3_task_3.py
+++
@@ -1,8 +0,0 @@
-from address import Address
-from mailing import Mailing
-
-to_address = Address(101000, "Москва", "Невская", 28, 10)
-from_address = Address(354000, "Сочи", "Ленина", 45, 13)
-mailing = Mailing(to_address, from_address, 3000, '81726354')
-
-print(mailing)
