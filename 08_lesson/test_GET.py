24bd4c6b88885c3499e6dc71b893f4cbc9b728af
Enot546 <vityusha.matveev.1983@mail.ru>
2026-05-11 02:14:30 +0800

    8.1

--- 08_lesson/test_GET.py
+++ 08_lesson/test_GET.py
@@ -20,3 +20,22 @@ def test_positive_get():
                             headers=headers)
     print(f"Status: {response.status_code}")
     print(response.json())
+
+
+def test_negative_no_token_get():
+    BASE_URL = "https://ru.yougile.com/api-v2"
+    fake_id = "00000000-0000-0000-0000-000000000000"
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer NoToken",
+    }
+
+    response = requests.get(f"{BASE_URL}/projects/{fake_id}",
+                            headers=headers)
+    print(f"Status: {response.status_code}")
+    print(response.json())
+
+    print("✅ Тест пройден: сервер вернул 404 для несуществующего проекта\n")
+    assert response.status_code == 401, f"Ожидался 401, получен {response.
+                                                                 status_code}"
--- 08_lesson/test_PUT.py
+++ 08_lesson/test_PUT.py
@@ -13,7 +13,7 @@ def test_positive_put():
 
     payload = {
         "deleted": False,
-        "title": "название проекта",
+        "title": "обновлённое название",
         "users": {
             '363be750-07ca-491b-b974-f59473364037': 'admin',
         }
@@ -29,3 +29,28 @@ def test_positive_put():
         print("✅ Проект обновлён!")
     else:
         print(f"❌ Ошибка {response.status_code}: {response.text}")
+
+
+def test_negative_put_invalid_role():
+    BASE_URL = "https://ru.yougile.com/api-v2"
+
+    payload = {
+        "deleted": False,
+        "title": "обновлённое название",
+        "users": {
+            '363be750-07ca-491b-b974-f59473364037': 'superadmin',
+        }
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {TOKEN}",
+    }
+    response = requests.put(f"{BASE_URL}/projects/{PROJECT_ID}", json=payload,
+                            headers=headers)
+
+    print(f"Status: {response.status_code}")
+    print(response.text)
+
+    assert response.status_code == 400, f"Ожидался 400, получен {response.
+                                                                 status_code}"
+    print("✅ Тест пройден: сервер вернул 400 при невалидной роли\n")
